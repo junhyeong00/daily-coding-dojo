2da9bc0a239bbd803c3c712b43757506dcbe95c5
junhyeong00 <jhbae0420@naver.com>
2022-08-01 09:22:41 +0900

    코드정리

--- 20220801/음양더하기/Solution.java
+++ 20220801/음양더하기/Solution.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
 
-        for (int i = 0; i < absolutes.length; i += 1){
-            if (signs[i] == true){
+        for (int i = 0; i < absolutes.length; i += 1) {
+            if (signs[i] == true) {
                 answer += absolutes[i];
                 continue;
             }
