d4418d0c38b9793db1b59bfce47272015c203c22
junhyeong00 <jhbae0420@naver.com>
2022-08-08 09:13:46 +0900

    코드 정리

--- 20220808/수박수박수/Solution.java
+++ 20220808/수박수박수/Solution.java
@@ -2,8 +2,8 @@ class Solution {
     public String solution(int n) {
         String answer = "";
 
-        for (int i = 0; i < n; i += 1){
-            if(i % 2 == 0){
+        for (int i = 0; i < n; i += 1) {
+            if (i % 2 == 0) {
                 answer += "수";
                 continue;
             }
