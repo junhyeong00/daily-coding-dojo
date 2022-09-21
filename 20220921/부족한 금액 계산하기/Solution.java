cccdf1bde4535d1fe38702f1f9134b152a188382
junhyeong00 <jhbae0420@naver.com>
2022-09-21 09:27:55 +0900

    수정

--- 20220921/부족한 금액 계산하기/Solution.java
+++ 20220921/부족한 금액 계산하기/Solution.java
@@ -1,2 +1,14 @@
-public class Solution {
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        long totalPrice = 0;
+
+        for (int i = 1; i <= count; i += 1) {
+            totalPrice += price * i;
+        }
+
+        answer *= money - totalPrice;
+
+        return answer < 0 ? 0 : answer;
+    }
 }
