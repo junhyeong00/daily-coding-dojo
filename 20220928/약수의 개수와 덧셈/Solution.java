1b3d9e4dbe9274c380038602a598b07b580ce1aa
junhyeong00 <jhbae0420@naver.com>
2022-09-28 09:21:50 +0900

    줄 변경

--- 20220928/약수의 개수와 덧셈/Solution.java
+++ 20220928/약수의 개수와 덧셈/Solution.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int left, int right) {
         int answer = 0;
 
-
         for (int i = left; i <= right; i += 1) {
             int numberOfFactors = 0;
+            
             for(int j = 1; j <= i; j += 1) {
                 if (i % j == 0) {
                     numberOfFactors += 1;
