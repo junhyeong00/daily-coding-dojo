2c4d2fb6ed8cab17f92123cf47fe181584f75703
junhyeong00 <jhbae0420@naver.com>
2022-09-29 09:03:45 +0900

    빈줄 제거

--- 20220928/약수의 개수와 덧셈/Solution.java
+++ 20220928/약수의 개수와 덧셈/Solution.java
@@ -4,7 +4,7 @@ class Solution {
 
         for (int i = left; i <= right; i += 1) {
             int numberOfFactors = 0;
-            
+
             for(int j = 1; j <= i; j += 1) {
                 if (i % j == 0) {
                     numberOfFactors += 1;
