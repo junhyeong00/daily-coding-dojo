57464c1a77f58d4fcc782a385edb94f6da97b9a9
junhyeong00 <jhbae0420@naver.com>
2022-10-19 10:13:26 +0900

    추상화 레벨 변경

--- 20221019/GymClothes/src/main/java/GymClothes.java
+++ 20221019/GymClothes/src/main/java/GymClothes.java
@@ -11,6 +11,30 @@ public class GymClothes {
 
 //        System.out.print(possession);
 
+        lendGymClothes(n, possession);
+
+        answer = CountNumberOfStudentsWhoCanAttend(n, answer, possession);
+
+//        System.out.print(possession);
+
+        return answer;
+    }
+
+    public void countGymClothes(int n, int[] lost, int[] reserve, Map<Integer, Integer> possession) {
+        for (int i = 1; i <= n; i += 1) {
+            possession.put(i, 1);
+        }
+
+        for (int student : lost) {
+            possession.put(student, possession.get(student) - 1);
+        }
+
+        for (int student : reserve) {
+            possession.put(student, possession.get(student) + 1);
+        }
+    }
+
+    public void lendGymClothes(int n, Map<Integer, Integer> possession) {
         for (int i = 1; i <= n; i += 1) {
             if (possession.get(i) != 0) {
                 continue;
@@ -31,29 +55,14 @@ public class GymClothes {
                 }
             }
         }
+    }
 
+    public int CountNumberOfStudentsWhoCanAttend(int n, int answer, Map<Integer, Integer> possession) {
         for (int i = 1; i <= n; i += 1) {
             if (possession.get(i) >= 1) {
                 answer += 1;
             }
         }
-
-//        System.out.print(possession);
-
         return answer;
-    }
-
-    public void countGymClothes(int n, int[] lost, int[] reserve, Map<Integer, Integer> possession) {
-        for (int i = 1; i <= n; i += 1) {
-            possession.put(i, 1);
-        }
-
-        for (int student : lost) {
-            possession.put(student, possession.get(student) - 1);
-        }
-
-        for (int student : reserve) {
-            possession.put(student, possession.get(student) + 1);
-        }
     }
 }
