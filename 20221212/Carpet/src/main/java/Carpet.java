0cb6af3484ba6e32ccc8f41551ff23f171c2b7b7
junhyeong00 <jhbae0420@naver.com>
2022-12-12 10:12:51 +0900

    20221212-카펫

---
+++ 20221212/Carpet/src/main/java/Carpet.java
@@ -0,0 +1,19 @@
+public class Carpet {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+
+        for (int i = 1; i < brown; i += 1) {
+            if (yellow == calcNecessaryYellow(brown, i)) {
+                answer[0] = brown / 2 - i;
+                answer[1] = i + 2;
+                return answer;
+            }
+        }
+
+        return answer;
+    }
+
+    public int calcNecessaryYellow(int brown, int i) {
+        return (brown / 2 - i - 2) * i;
+    }
+}
