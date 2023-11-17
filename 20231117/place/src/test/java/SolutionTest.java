e29f698730d39a62b9506c83021e145949f47ca6
junhyeong00 <jhbae0420@naver.com>
2023-11-17 17:32:51 +0900

    20231117-거리두기 확인하기

---
+++ 20231117/place/src/test/java/SolutionTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    private Solution test;
+
+    @BeforeEach
+    void setUp() {
+        test = new Solution();
+    }
+
+    @Test
+    void solution() {
+        assertArrayEquals(new int[]{1, 0, 1, 1, 1},
+                test.solution(new String[][]{
+                        {"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
+                        {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
+                        {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
+                        {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
+                        {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}}));
+    }
+}
